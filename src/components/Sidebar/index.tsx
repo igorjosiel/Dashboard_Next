baa272f4595356ce5b5b47f5960de65cabd4b7d8
Igor Josiel <igorjosielvitalino@gmail.com>
2022-08-17 18:01:39 -0300

    fix: finalizando a refatoração dos componentes.

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Stack, Text, Link, Icon } from "@chakra-ui/react";
 import { RiDashboardLine, RiContactsLine, RiInputMethodLine, RiGitMergeLine } from "react-icons/ri";
+import NavLink from "./NavLink";
 import NavSection from "./NavSection";
 
 export function Sidebar() {
@@ -7,21 +8,12 @@ export function Sidebar() {
         <Box as="aside" w="64" mr="8">
             <Stack spacing="12" align="flex-start">
                 <NavSection title="GERAL">
-                  
-                  <Link display="flex" alignItems="center">
-                    <Icon as={RiGitMergeLine} fontSize="20" />
-                    <Text ml="4" fontWeight="medium">Automação</Text>
-                  </Link>
+                  <NavLink icon={RiDashboardLine} text={'Dashboard'} />
+                  <NavLink icon={RiContactsLine} text={'Usuários'} />
                 </NavSection>
                 <NavSection title="AUTOMAÇÂO">
-                  <Link display="flex" alignItems="center">
-                    <Icon as={RiInputMethodLine} fontSize="20" />
-                    <Text ml="4" fontWeight="medium">Formulários</Text>
-                  </Link>
-                  <Link display="flex" alignItems="center">
-                    <Icon as={RiGitMergeLine} fontSize="20" />
-                    <Text ml="4" fontWeight="medium">Automação</Text>
-                  </Link>
+                  <NavLink icon={RiInputMethodLine} text={'Formulários'} />
+                  <NavLink icon={RiGitMergeLine} text={'Automação'} />
                 </NavSection>
 
                 <Box>
